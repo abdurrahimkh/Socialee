0ab5ac1a7a8743ccad3ac44615ec4727219fee45
abdurrahimkh <abdurrahimkhan9574@gmail.com>
2022-11-16 12:32:25 +0500

    project setup

---
+++ server.js
@@ -0,0 +1,20 @@
+// Global Imports
+const express = require("express");
+
+// Project Import
+const database = require("./config/database");
+const viewsRouter = require("./routes/Views");
+
+// Database connection
+database.connect();
+
+// Fire up server
+const app = express();
+
+// Routes
+app.use("/", viewsRouter);
+
+// Listen to Server
+app.listen(5000, () => {
+  console.log(`Server running at PORT: 5000`);
+});
